e58de2b600aa3d8b9098c8fc5b13c646ad4de11e
Ron Nahmias <ron.nahmias2@gmail.com>
2020-10-31 23:59:15 +0200

    switch position function + change to new odd range

--- hw2.py
+++ hw2.py
@@ -2,6 +2,12 @@
 # GOOD LUCK!
 
 # ************************ QUESTION 1 **************************
+def rhombus(side_size):
+    # this function print rhombus vertically using help function that prints 2 triangle in opposite direction
+    print_triangle(side_size, 1, side_size, 1)
+    print_triangle(side_size, side_size, 0, -1)
+
+
 def print_triangle(size, start, end, direction):
     # this function print triangle in vertical direction with given the size of the rows and the direction
     asterisk = "*"
@@ -11,13 +17,21 @@ def print_triangle(size, start, end, direction):
         print(back_space * (size - i) + asterisk * (2 * i - 1) + back_space * (size - i))
 
 
-def rhombus(side_size):
-    # this function print rhombus vertically using help function that prints 2 triangle in opposite direction
-    print_triangle(side_size, 1, side_size, 1)
-    print_triangle(side_size, side_size, 0, -1)
-
-
 # ************************ QUESTION 2 **************************
+def lagrange_four_square_theorem(num):
+    lagrange_list = []
+    for i in range(int(num ** 0.5) + 1):
+        for j in range(int(num ** 0.5) + 1):
+            for k in range(int(num ** 0.5) + 1):
+                for m in range(int(num ** 0.5) + 1):
+                    if ((i ** 2) + (j ** 2) + (k ** 2) + (m ** 2)) == num:
+                        lagrange_list.append([i, j, k, m])
+    for small_list in lagrange_list:
+        sort_list_min_to_max(small_list)
+    lagrange_list = remove_dup_lists(lagrange_list)
+    return lagrange_list
+
+
 def sort_list_min_to_max(list_numbers):
     for i in range(1, len(list_numbers)):
         for j in range(0, len(list_numbers) - i):
@@ -42,22 +56,15 @@ def sort_sum_nested_list(list_numbers):
     return list_numbers
 
 
-def lagrange_four_square_theorem(num):
-    lagrange_list = []
-    for i in range(int(num ** 0.5) + 1):
-        for j in range(int(num ** 0.5) + 1):
-            for k in range(int(num ** 0.5) + 1):
-                for m in range(int(num ** 0.5) + 1):
-                    if ((i ** 2) + (j ** 2) + (k ** 2) + (m ** 2)) == num:
-                        lagrange_list.append([i, j, k, m])
-    for small_list in lagrange_list:
-        sort_list_min_to_max(small_list)
-    lagrange_list = remove_dup_lists(lagrange_list)
-    lagrange_list = sort_sum_nested_list(lagrange_list)
-    return lagrange_list
-
-
 # ************************ QUESTION 3 **************************
+def max_series(numbers):
+    # This function uses 2 functions of odd series and divided in 3. The function return the value as a list of  results
+    if not numbers:
+        return numbers
+    res = [odd_range(numbers), max_number_divide_3(numbers)]
+    return res
+
+
 def sort_list_max_to_min(list_numbers):
     for i in range(1, len(list_numbers)):
         for j in range(0, len(list_numbers) - i):
@@ -77,33 +84,18 @@ def max_number_divide_3(numbers):
     return max_num
 
 
-def odd_range(numbers):
-    # This function get list of numbers and return the length of longest series of odd numbers in the list
-    series_counter = long_series = 0
-    if numbers[0] % 2 != 0:
-        long_series += 1
-        series_counter += 1
-    for i in range(1, len(numbers)):
-        if numbers[i] % 2 != 0 and numbers[i] > numbers[i - 1]:
-            series_counter += 1
-        elif numbers[i] % 2 != 0:
-            series_counter = 1
+def odd_range(list):
+    counter = max = 0
+    for i in range(len(list)):
+        if list[i] % 2 != 0:
+            if i == 0:
+                counter += 1
+            elif list[i] > list[i - 1]:
+                counter += 1
+            else:
+                counter = 1
         else:
-            series_counter = 0
-        # checks if the size of the series that we have found is bigger than what we have found
-        if series_counter > long_series and series_counter > 1:
-            long_series = series_counter
-        # if the number is even - we init the counter for the next iteration
-    if series_counter > long_series and series_counter > 1:
-        long_series = series_counter
-    if long_series == 1:
-        long_series = 0
-    return long_series
-
-
-def max_series(numbers):
-    # This function uses 2 functions of odd series and divided in 3. The function return the value as a list of  results
-    if not numbers:
-        return numbers
-    res = [odd_range(numbers), max_number_divide_3(numbers)]
-    return res
+            counter = 0
+        if counter > max:
+            max = counter
+    return max
